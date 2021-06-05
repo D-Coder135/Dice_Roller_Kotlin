b91f7a942bea9219d3b3e39b70da6c5e88247810
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-05 12:18:04 +0530

    add some contents inside the body of the when block

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -50,7 +50,13 @@ class MainActivity : AppCompatActivity() {
 //        }
 
         when (diceRoll) {
-
+            luckyNumber -> println("You won!")
+            1 -> println("So sorry! You rolled a 1. Try again!")
+            2 -> println("Sadly, you rolled a 2. Try again!")
+            3 -> println("Unfortunately, you rolled a 3. Try again!")
+            4 -> println("No luck! You rolled a 4. Try again!")
+            5 -> println("Don't cry! You rolled a 5. Try again!")
+            6 -> println("Apologies! you rolled a 6. Try again!")
         }
 
         // Update the screen with the dice roll
