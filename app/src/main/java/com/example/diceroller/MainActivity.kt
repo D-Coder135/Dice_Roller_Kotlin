3e6df144951c4282e854af755ffccbfd8ac97cc5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-05 13:52:51 +0530

    initialize a field diceImage with the reference of the ImageView palette by calling the findById() method

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -62,6 +63,8 @@ class MainActivity : AppCompatActivity() {
         // Update the screen with the dice roll
 //        val resultTextView: TextView = findViewById(R.id.textView)
 //        resultTextView.text = diceRoll.toString()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
     }
 }
 
